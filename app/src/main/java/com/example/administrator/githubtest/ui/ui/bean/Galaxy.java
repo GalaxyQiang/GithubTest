0921bfa0ea1f747acf6711943d06eecb5650fe78
John <galaxyqiang@gmail.com>
2018-02-14 16:58:09 +0800

    添加星系类到bean文件夹下，移动mainactivity到ui文件夹下

---
+++ app/src/main/java/com/example/administrator/githubtest/ui/ui/bean/Galaxy.java
@@ -0,0 +1,24 @@
+package com.example.administrator.githubtest.ui.ui.bean;
+
+/**
+ * Created by Administrator on 2018/2/14.
+ */
+
+public class Galaxy {
+    private String mName;
+
+    public Galaxy(String mName) {
+        this.mName = mName;
+    }
+
+    public Galaxy() {
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+}
--- app/src/main/java/com/example/administrator/githubtest/ui/ui/ui/MainActivity.java
+++ app/src/main/java/com/example/administrator/githubtest/ui/ui/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.administrator.githubtest;
+package com.example.administrator.githubtest.ui.ui.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.administrator.githubtest.R;
 
 public class MainActivity extends AppCompatActivity {
 
